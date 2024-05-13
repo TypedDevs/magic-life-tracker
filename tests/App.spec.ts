47130c05cfaf361da16bdf051f5a0a5ed974743c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-05-13 20:05:10 +0200

    chore: refactor join test to cover full visual

--- tests/App.spec.ts
+++ tests/App.spec.ts
@@ -3,17 +3,9 @@ import { test, expect } from 'vitest'
 import { render } from '@testing-library/vue'
 import App from '@/App.vue'
 
-test('displays the default lives of two players', () => {
+test('displays the default life tracker for two players', () => {
   const { getAllByText } = render(App)
   expect(getAllByText('20').length).toBe(2)
-})
-
-test('displays the decrease live buttons', () => {
-  const { getAllByText } = render(App)
   expect(getAllByText('-').length).toBe(2)
-})
-
-test('displays the increase live buttons', () => {
-  const { getAllByText } = render(App)
   expect(getAllByText('+').length).toBe(2)
 })
